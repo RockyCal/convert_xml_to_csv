bbdbd8b22248dad10d5f079ef8a3719cfc1ac125
Raquel <calderon.rocky@gmail.com>
2015-03-10 17:21:26 -0700

    Dublin core works

--- convert.py
+++ convert.py
@@ -8,6 +8,11 @@ from bs4 import BeautifulSoup
 import re
 import csv
 
+
+class Record:
+    def __init__(self, file_id):
+        self.file_id = file_id
+
 namespaces = {'gmi': "http://www.isotc211.org/2005/gmi", 'gmd': "http://www.isotc211.org/2005/gmd", 'gco':
               'http://www.isotc211.org/2005/gco', 'gml': "http://www.opengis.net/gml/3.2", 'gmx':
               "http://www.isotc211.org/2005/gmx", 'gsr': "http://www.isotc211.org/2005/gsr",
@@ -15,54 +20,69 @@ namespaces = {'gmi': "http://www.isotc211.org/2005/gmi", 'gmd': "http://www.isot
               'xlink': "http://www.w3.org/1999/xlink", 'xsi': "http://www.w3.org/2001/XMLSchema-instance",
               'saxon': "http://saxon.sf.net/", 'srv': "http://www.isotc211.org/2005/srv",
               'schemaLocation': "http://www.isotc211.org/2005/gmi http://www.ngdc.noaa.gov/metadata"
-                                "/published/xsd/schema.xsd"}
+              "/published/xsd/schema.xsd"}
 
 hydro10 = "http://hydro10.sdsc.edu/"
-#source = "http://hydro10.sdsc.edu/metadata/"
+# source = "http://hydro10.sdsc.edu/metadata/"
 
 dublin_core = "http://dublincore.org/documents/dcmi-terms/"
 
 fieldnames = ['name', 'description', 'resource url', 'keywords', 'defining citation', 'related to',
               'parent organization', 'abbreviation', 'synonyms', 'funding info']
 
-def parse_dublin_core(root, writer):
-    name = root.find('{http://purl.org/dc/elements/1.1/}title').text
-    url = root.find('{http://purl.org/dc/elements/1.1/}identifier').text
-    description = root.find('{http://purl.org/dc/elements/1.1/}description')
+
+def parse_dublin_core(root, record, writer):
+    print(record.file_id)
+    dc = root.find('{http://www.w3.org/1999/02/22-rdf-syntax-ns#}Description')
+    record.name = dc.find('{http://purl.org/dc/elements/1.1/}title')
+    record.url = dc.find('{http://purl.org/dc/elements/1.1/}identifier').text
+    record.description = dc.find('{http://purl.org/dc/elements/1.1/}description').text
     keywords = []
-    for each in root.findall('{http://purl.org/dc/elements/1.1/}subject'):
-        keywords.append(each.text)
-    if root.find('{http://purl.org/dc/elements/1.1/}bibliographicCitation'):
-        defining_citation = root.find('{http://purl.org/dc/elements/1.1/}bibliographicCitation').text
+    for each in dc.findall('{http://purl.org/dc/elements/1.1/}subject'):
+        print(each.text)
+        if each.text is not None:
+            keywords.append(each.text)
+    if len(keywords) > 1:
+        record.keywords = ', '.join(keywords)
     else:
-        defining_citation = ""
-    if root.find('{http://purl.org/dc/elements/1.1/}references'):
-        related_to = root.find('{http://purl.org/dc/elements/1.1/}references').text
+        record.keywords = keywords
+    if dc.find('{http://purl.org/dc/elements/1.1/}bibliographicCitation'):
+        record.defining_citation = dc.find('{http://purl.org/dc/elements/1.1/}bibliographicCitation').text
     else:
-        related_to = ""
-    writer.writerow({'name': name, 'resource url': url, 'description': description, 'keywords': ', '.join(keywords),
-                     'defining citation': defining_citation, 'related to': related_to, 'parent organization': '',
-                     'abbreviation': '', 'synonyms': '', 'funding info':''})
+        record.defining_citation = ""
+    if dc.find('{http://purl.org/dc/elements/1.1/}references'):
+        record.related_to = dc.find('{http://purl.org/dc/elements/1.1/}references').text
+    else:
+        record.related_to = ""
+    writer.writerow({'name': record.name, 'resource url': record.url, 'description': record.description, 'keywords':
+                     record.keywords, 'defining citation': record.defining_citation, 'related to':
+                     record.related_to, 'parent organization': '', 'abbreviation': '', 'synonyms': '',
+                     'funding info': ''})
 
 
 def search_dir(request):
-    resource_title = re.sub('http://hydro10.sdsc.edu/metadata/', '', request.full_url)
-    with open('{}.csv'.format(resource_title)) as csvfile:
+    resource_title = re.sub('http://hydro10.sdsc.edu/metadata/', '', request.full_url).strip('/ ')
+    with open('{}.csv'.format(resource_title), 'w') as csvfile:
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-    writer.writeheader()
-    curr_dir = BeautifulSoup(urlopen(request))
-    dir_link = request.full_url
-    for file in curr_dir.find_all('a'):
-        if file['href'] != '/metadata/':
-            file_link = urljoin(dir_link, file['href'])
-            try:
-                eTree.parse(urlopen(file_link))
-            except ParseError as e:
-                print("{}; {}".format(e.msg, request.full_url))
-            tree = eTree.parse(urlopen(file_link))
-            root = tree.getroot()
-            if root.tag == "{http://www.w3.org/1999/02/22-rdf-syntax-ns#}RDF":
-                parse_dublin_core(root, writer)
+        writer.writeheader()
+        curr_dir = BeautifulSoup(urlopen(request))
+        dir_link = request.full_url
+        for file in curr_dir.find_all('a'):
+            record = Record(file.text)
+            if file['href'] != '/metadata/':
+                file_link = urljoin(dir_link, file['href'])
+                try:
+                    eTree.parse(urlopen(file_link))
+                except ParseError as e:
+                    print("{}; {}".format(e.msg, request.full_url))
+                tree = eTree.parse(urlopen(file_link))
+                root = tree.getroot()
+                if root.tag == "{http://www.w3.org/1999/02/22-rdf-syntax-ns#}RDF":
+                    parse_dublin_core(root, record, writer)
+                #elif root.tag == "{http://www.isotc211.org/2005/gmd}MD_Metdata" or root.tag == "{http://www.isotc211.org/" \
+                #                                                                              "2005/gmi}MI_Metdata":
+                # parse_iso(root, writer)
+
 
 def search_from_source(source):
     soup = BeautifulSoup(urlopen(source))
@@ -83,22 +103,21 @@ def search_from_source(source):
             # Each 'file' is a http link to xml file
             search_dir(req)
 
+
 """Initialize by asking user for type of input and link to it"""
 while True:
-  try: 
-    choice = input("Would you like to run through a single (d)irectory or a (s)ource with many directories?")
-  except ValueError:
-    print("Sorry, could not understand that, please enter 'd' or 's' ")
-    continue
-  if choice != 's' and choice != 'd':
-    print("Not a valid option, please enter 'd' or 's' ")
-    continue
-  else:
-    break
-if choice == 's':
-    source_link = input("Please provide the source url: ")
-    search_from_source(source_link)
-elif choice == 'd':
-    directory_link = input("Please provide the directory url: ")
-    dir_request = Request(directory_link)
-    search_dir(dir_request)
+    try:
+        choice = input("Would you like to run through a single (d)irectory or a (s)ource with many directories?")
+    except ValueError:
+        print("Sorry, could not understand that, please enter 'd' or 's' ")
+        continue
+    if choice != 's' and choice != 'd':
+        print("Not a valid option, please enter 'd' or 's' ")
+        continue
+    elif choice == 's':
+        source_link = input("Please provide the source url: ")
+        search_from_source(source_link)
+    elif choice == 'd':
+        directory_link = input("Please provide the directory url: ")
+        dir_request = Request(directory_link)
+        search_dir(dir_request)
